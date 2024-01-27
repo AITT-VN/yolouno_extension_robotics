7549cdcfe8c04cc9e8216b163c93b7a8af66c93e
Minh <vanminh0910@gmail.com>
2024-01-27 07:21:02 +0700

    Add missing file in config

--- robotics_motor.py
+++ robotics_motor.py
@@ -1,10 +1,15 @@
+import asyncio
 from machine import PWM, Pin
 from i2c_motors_driver import MotorDriver
+import robotics_motor_driver_v2 as mdv2
 from utility import *
 
 class DCMotor:
     def __init__(self, reversed=False):
-        self._reversed = reversed
+        if reversed:
+            self._reversed = -1
+        else:
+            self._reversed = 1
 
     def speed(self, value):    
         pass
@@ -13,24 +18,156 @@ class DCMotor:
         pass
 
     def reverse(self):
-        self._reversed = not self._reversed
-
+        if self._reversed == 1:
+            self._reversed = -1
+        else:
+            self._reversed = 1
 
 class DCMotorI2CV1 (DCMotor):
-    def __init__(self, driver: MotorDriver, index, reversed=False):
+    def __init__(self, driver: MotorDriver, port, reversed=False):
         self._driver = driver
-        self._index = index
+        self._port = port
         super().__init__(reversed)
 
     def speed(self, value):
         value = int(max(min(100, value),-100))
-        if self._reversed:
-            self._driver.setSpeed(self._index, -value)
-        else:
-            self._driver.setSpeed(self._index, value)
+        self._driver.setSpeed(self._port, value*self._reversed)
     
     def brake(self):
         self.speed(0)
+
+class DCMotorI2CV2 (DCMotor):
+    def __init__(self, driver: mdv2.MotorDriver, port, reversed=False):
+        self._driver = driver
+        self._port = port
+        self._ppr = 4680
+        super().__init__(reversed)
+    
+    '''
+        Gets the rotation angle of the motor.
+
+        Returns: Motor angle.
+    '''
+    def angle(self):
+        if self._port not in (mdv2.MOTOR_E1, mdv2.MOTOR_E2):
+            return 0
+
+        ticks = self._driver.get_encoder(self._port)
+        rotations = ticks/self._ppr
+        return rotations * 360 * self._reversed
+
+    '''
+        Sets the accumulated rotation angle of the motor to 0.
+    '''
+    def reset_angle(self):
+        if self._port not in (mdv2.MOTOR_E1, mdv2.MOTOR_E2):
+            return
+        self._driver.reset_encoder(self._port)
+
+    '''
+        Sets or gets the speed of the motor.
+
+        The speed is measured as the change in the motor angle during last 100ms. 
+        
+        Parameters:
+            speed (Number, %) - Speed of the motor if set.
+
+        Returns:
+            Motor speed (rpm).
+    '''
+    def speed(self, value=None):
+        if value == None:
+            if self._port not in (mdv2.MOTOR_E1, mdv2.MOTOR_E2):
+                return 0
+            return self._driver.get_speed(self._port)*self._reversed/self._ppr
+        else:
+            value = int(max(min(100, value),-100))
+            self._driver.set_motor(self._port, value*self._reversed)
+
+
+    ############## Stopping #################
+
+    '''
+        Passively brakes the motor.
+
+        The motor stops due to friction, plus the voltage that is generated while the motor is still moving.
+    '''
+    def brake(self):
+        self._driver.brake(self._port)
+        if self._port == mdv2.MOTOR_E1 or self._port == mdv2.MOTOR_E2:
+            self._driver.reset_encoder(self._port)
+    
+    
+    '''
+        Stops the motor using given method.
+    '''
+    async def stop_then(self, then):
+        if then == mdv2.STOP_COAST:
+            self.speed(0)
+        elif then == mdv2.STOP_BRAKE:
+            self.brake()
+            #await asyncio.sleep_ms(200)
+            #self.speed(0)
+        elif then == mdv2.STOP_HOLD:
+            # TODO: Support stop hold position
+            pass
+        else: # None: Do nothing when approaching the target position
+            return
+    
+    '''
+        Runs the motor at a constant speed towards a given target angle.
+
+        The direction of rotation is automatically selected based on the target angle. It does not matter if speed is positive or negative.
+
+        Parameters:
+            speed (Number, %) - Speed of the motor.
+
+            angle (Number, deg) - Angle by which the motor should rotate.
+
+            then (Stop) - What to do after coming to a standstill.
+
+    '''
+    async def run_angle(self, speed, angle, then=mdv2.STOP_COAST):
+        if self._port not in (mdv2.MOTOR_E1, mdv2.MOTOR_E2):
+            return
+
+        self.reset_angle()
+
+        self.speed(speed)
+
+        while abs(self.angle()) < abs(angle):
+            await asyncio.sleep_ms(5)
+
+        await self.stop_then(then)
+    
+    '''
+        Runs the motor at a constant speed towards a given target rotations.
+
+        The direction of rotation is automatically selected based on the target rotations. It does not matter if speed is positive or negative.
+
+        Parameters:
+            speed (Number, %) - Speed of the motor.
+
+            rotation (Number, int) - Number rotation by which the motor should rotate.
+
+            then (Stop) - What to do after coming to a standstill.
+
+    '''
+    async def run_rotation(self, speed, rotation, then=mdv2.STOP_COAST):
+        if self._port not in (mdv2.MOTOR_E1, mdv2.MOTOR_E2):
+            return
+
+        self.reset_angle()
+
+        self.speed(speed)
+
+        while (abs(self.angle())/360) < abs(rotation):
+            print(abs(self.angle())/360)
+            await asyncio.sleep_ms(5)
+
+        await self.stop_then(then)
+        print(abs(self.angle())/360)
+
 
 class DCMotor2PIN (DCMotor):
     def __init__(self, in1, in2, reversed=False):
@@ -41,6 +178,8 @@ class DCMotor2PIN (DCMotor):
 
     def speed(self, value):
         value = int(max(min(100, value),-100))
+
+        value = value*self._reversed
 
         if value > 0:
             # Forward
@@ -73,6 +212,7 @@ class DCMotor3PIN (DCMotor):
 
     def speed(self, value):
         value = int(max(min(100, value),-100))
+        value = value*self._reversed
 
         if value > 0:
             # Forward
